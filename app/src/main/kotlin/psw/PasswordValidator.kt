aee329197e38baaa5fc32b0773c98388a5016a04
Paco Benitez <paco.benitez@sfd.gov.sa>
2022-06-24 15:11:28 +0200

    Rest of validation

--- app/src/main/kotlin/psw/PasswordValidator.kt
+++ app/src/main/kotlin/psw/PasswordValidator.kt
@@ -3,12 +3,17 @@ package psw
 class PasswordValidator {
     fun valid(pass: String): Boolean {
         if (!validLength(pass)) return false
-        if (!hasNotUpperCase(pass)) return false
+        if (!hasUpperCase(pass)) return false
+        if (!hasNumber(pass)) return false
+        if (!hasUnderscore(pass)) return false
         return true
     }
 
-    private fun hasNotUpperCase(pass: String) = pass.contains("[A-Z]".toRegex())
+    private fun hasUpperCase(pass: String) = pass.contains("[A-Z]".toRegex())
 
-    private fun validLength(pass: String): Boolean = pass.length > 8
+    private fun validLength(pass: String) = pass.length > 8
 
+    private fun hasNumber(pass: String) = pass.contains("[0-9]".toRegex())
+
+    private fun hasUnderscore(pass: String) = pass.contains("_")
 }
